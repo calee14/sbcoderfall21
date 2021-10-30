d8a9f0a42a9d00a58ef1605706febdb2f5ad64c4
Ron Kibel <ron.kibel1@gmail.com>
2021-10-29 18:22:35 -0700

    Update Board.py

--- backend-chess/Board.py
+++ backend-chess/Board.py
@@ -1,4 +1,5 @@
 import pygame
+from Piece import Piece
 from PIL import Image
 
 class Board:
@@ -7,6 +8,7 @@ class Board:
         self.BORDER = border
         self.name = name
         self.resize()
+        self.pieces = [[None]*8 for i in range(8)]
 
     def resize(self):
         import_board = Image.open(self.name)
@@ -31,6 +33,13 @@ class Board:
             rect = (self.BORDER + x*self.DIM/8, self.BORDER + y*self.DIM/8, self.DIM/8, self.DIM/8)
             pygame.draw.rect(screen, (255, 0, 0, 50), rect, 2)
 
+    def print_board(self):
+        print(self.pieces)
 
+    # @param Piece object and chess notation position (string)
+    def add_piece(self, screen, piece):
+        x, y = piece.translate_pos_matrix()
+        self.pieces[x][y] = piece
+        piece.load(screen)
 
 
