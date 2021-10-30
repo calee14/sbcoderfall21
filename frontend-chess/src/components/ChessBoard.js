f0b720c0f90034656c9bdfee95b01e39e8f6b181
calee14 <caplee1000@gmail.com>
2021-10-29 17:53:30 -0700

    init board with blocks

--- frontend-chess/src/components/ChessBoard.js
+++ frontend-chess/src/components/ChessBoard.js
@@ -1,11 +1,41 @@
 import React from 'react'
+import Block from './Block'
+import Piece from './Piece';
+import { useState } from 'react';
 import './ChessBoard.css'
 
-function ChessBoard() {
+function ChessBoard(props) {
+
+    var initBoard = [];
+    for(var i=0;i<8;i++) {
+        var row = [];
+        for(var j=0;j<8;j++) {
+            if(i%2==0) {
+                var color = (j % 2 == 0) ? 'white' : 'black';
+            } else {
+                var color = (j % 2 == 1) ? 'white' : 'black';
+            }
+            
+            row.push(new Piece(color, null, [i, j]));
+        }
+        initBoard.push(row);
+    }
+
+    const [board, setBoard] = useState(initBoard);
+    
+    console.log(board)
+    for(var i=0;i<8;i++) {
+        for(var j=0;j<8;j++) {
+            console.log(board[i][j].color);
+        }
+    }
+
     return (
     <>
         <div className="board-container">
-            <img className="chess-board" src="https://upload.wikimedia.org/wikipedia/commons/1/13/Animation_showing_windmill_%28aka_seesaw%29_chess_tactic.gif"></img>
+            <div className="chess-board">
+                { board.map((row) => row.map((piece) => <Block key={piece.pos} piece={piece}/>)) }
+            </div>
         </div>
     </>
     );
