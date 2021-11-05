6ab6faa89444205523e528639026e0155e848f18
calee14 <caplee1000@gmail.com>
2021-11-04 19:40:02 -0700

    block comp. for render chess piece

--- frontend-chess/src/components/Block.js
+++ frontend-chess/src/components/Block.js
@@ -1,11 +1,30 @@
-import React from "react";
+import React, { useState } from "react";
 import './ChessBoard.css'
 
 function Block(props) {
-    const {piece} = props;
+    const {square} = props;
+    const [isClicked, setClicked] = useState(false)
+    
+    function handleClicked(e) {
+        e.preventDefault();
+        console.log('You clicked a square.');
+        console.log(square.pos);
+        setClicked(!isClicked);
+        console.log(isClicked);
+    }
+
+    var img;
+    if(square.pieceType != null) {
+        img = <img src={square.pieceType.imgURL} width={80} height={80}></img>
+    } else {
+        img = <img></img>
+    }
+
     return (
         <>
-            <div className={`board-block ${piece.color}`}></div>
+            <div className={`board-block ${square.squareColor}`} onClick={handleClicked}>
+                {img}
+            </div>
         </>
     );
 }
