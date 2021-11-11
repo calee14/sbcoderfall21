dae861cfe886c3b8ae8fe8a5a0a1a157b3bde20c
calee14 <caplee1000@gmail.com>
2021-11-10 17:53:00 -0800

    render temp piece on screen

--- frontend-chess/src/components/ChessPieces/TempPiece.js
+++ frontend-chess/src/components/ChessPieces/TempPiece.js
@@ -1,8 +1,21 @@
 import React from "react";
 
 function TempPiece(props) {
-    const {} = props;
+    const {mousePos, piece} = props;
+    console.log(`${mousePos}`)
     
+    const mouseStyle = {
+        position: 'absolute',
+        top: mousePos.y,
+        left: mousePos.x,
+    }
+    return (
+        <>
+            <div className={`chess-piece-temp`} style={mouseStyle}>
+                <img className="selectDisable" src={piece.imgURL} width={80} height={80}></img>
+            </div>
+        </>
+    );
 }
 
 export default TempPiece;
