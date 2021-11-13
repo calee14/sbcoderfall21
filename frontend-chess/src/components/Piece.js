40c26b47ca97527eccc3e02c7419c050665cbc55
calee14 <caplee1000@gmail.com>
2021-11-13 12:27:39 -0800

    add move hist to piece class

--- frontend-chess/src/components/Piece.js
+++ frontend-chess/src/components/Piece.js
@@ -5,11 +5,15 @@ class Piece {
         this.pieceColor = color;
         this.pos = pos;
         this.imgURL = imgURL// img file name
+        this.moveHistory = []
     }
 
     getImage() { return this.imgURL; }
     getPos() { return this.pos; }
     getPieceColor() { return this.pieceColor; }
+    getMoveHistory() { return this.moveHistory; }
+    
+    addMoveHistory(pos) { this.moveHistory.push(pos); }
 }
 
 export default Piece
