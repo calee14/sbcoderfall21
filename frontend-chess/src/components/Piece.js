d878c9866c3fcb8fbddcd4792fde88332d387bab
calee14 <caplee1000@gmail.com>
2021-11-13 15:37:33 -0800

    add helper funcs for piece detection and board

--- frontend-chess/src/components/Piece.js
+++ frontend-chess/src/components/Piece.js
@@ -1,4 +1,5 @@
 import React from "react";
+import Square from "./Square";
 
 class Piece {
     constructor(color, pos, imgURL="") {
@@ -12,8 +13,33 @@ class Piece {
     getPos() { return this.pos; }
     getPieceColor() { return this.pieceColor; }
     getMoveHistory() { return this.moveHistory; }
-    
-    addMoveHistory(pos) { this.moveHistory.push(pos); }
+
+    addMoveHistory(pos) { // store history of movement and update current pos
+        this.moveHistory.push(pos); 
+        this.pos = pos;
+    }
+
+    hasOppPieceColor(board, pos) {
+        if(pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) { return false; }
+        const sqaureAtPos = board[pos[0]][pos[1]];
+        console.log('reach func')
+        if(sqaureAtPos.getPieceType().getPieceColor() != this.pieceColor) {
+            console.log('has opposite color')
+            return true;
+        }
+        console.log('has same color')
+        return false;
+    }
+    pieceExists(board, pos) {
+        if(pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) { return false; }
+        const sqaureAtPos = board[pos[0]][pos[1]];
+        if(sqaureAtPos.getPieceType() != null) {
+            return true;
+        }
+        return false;
+    }
+
+    getMovementOptions(board=null, playerPiece=true) { return []; }
 }
 
 export default Piece
