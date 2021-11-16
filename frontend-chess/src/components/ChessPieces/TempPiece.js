8fb49d896d2c99f343a442b14cb3051fba6bed42
calee14 <caplee1000@gmail.com>
2021-11-15 21:39:07 -0800

    remove comment for smoother mouse interaction

--- frontend-chess/src/components/ChessPieces/TempPiece.js
+++ frontend-chess/src/components/ChessPieces/TempPiece.js
@@ -2,7 +2,6 @@ import React from "react";
 
 function TempPiece(props) {
     const {mousePos, piece} = props;
-    console.log(`${mousePos}`)
     
     const mouseStyle = {
         position: 'absolute',
