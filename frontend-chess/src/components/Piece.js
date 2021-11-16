4a3d9c3a36b2faef60b1702c4d87a908b5b0cc37
calee14 <caplee1000@gmail.com>
2021-11-15 18:15:12 -0800

    add out of bounds condition

--- frontend-chess/src/components/Piece.js
+++ frontend-chess/src/components/Piece.js
@@ -26,6 +26,7 @@ class Piece {
         if(pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) { return false; }
         const sqaureAtPos = board[pos[0]][pos[1]];
         console.log('reach func')
+        if(sqaureAtPos.getPieceType() == null) { return false; }
         if(sqaureAtPos.getPieceType().getPieceColor() != this.pieceColor) {
             console.log('has opposite color')
             return true;
@@ -33,6 +34,12 @@ class Piece {
         console.log('has same color')
         return false;
     }
+
+    outOfBounds(pos) {
+        if(pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) { return true; }
+        return false;
+    }
+
     pieceExists(board, pos) {
         if(pos[0] < 0 || pos[0] > 7 || pos[1] < 0 || pos[1] > 7) { return false; }
         const sqaureAtPos = board[pos[0]][pos[1]];
