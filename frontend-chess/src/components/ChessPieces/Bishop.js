b589ee02fa16b6f51c2ecf89556127b2bb5e6d5b
calee14 <caplee1000@gmail.com>
2021-11-15 18:15:22 -0800

    add movement for bishop

--- frontend-chess/src/components/ChessPieces/Bishop.js
+++ frontend-chess/src/components/ChessPieces/Bishop.js
@@ -5,9 +5,35 @@ class Bishop extends Piece {
         super(color, pos, imgURL);
     }
 
-    showMovement(board=null) {
-        var arr= [];
-        return;
+    getMovementOptions(board=null, playerPiece=true) {
+        /*
+            Parameters:
+                board: Array[Square]
+                playerPiece: bool
+            Returns: Array[] of the positions on the board
+        */
+
+        const pos = this.getPos();
+        console.log(pos);
+        var pieceRow = pos[0];
+        var pieceCol = pos[1];
+
+        var moveOptions = [];
+        
+        // find squares where bishop can move
+        const dirOptions = [[1, 1], [1, -1], [-1, -1], [-1, 1]];
+        for(var i=0;i<dirOptions.length;i++) {
+            var newPos = [pieceRow-dirOptions[i][0], pieceCol-dirOptions[i][1]];
+            while(!this.outOfBounds(newPos) && !this.pieceExists(board, newPos)) {
+                console.log(newPos);
+                moveOptions.push(newPos);
+                newPos = [newPos[0]-dirOptions[i][0], newPos[1]-dirOptions[i][1]];
+            }
+            if(!this.outOfBounds(newPos) && this.pieceExists(board, newPos) && this.hasOppPieceColor(board, newPos)) {
+                moveOptions.push(newPos);
+            }
+        }
+        return moveOptions;
     }
 }
 
