cbeb72465520f2f62eaf84c8f5bf33f054c78515
calee14 <caplee1000@gmail.com>
2021-11-15 18:33:24 -0800

    complete movement for knight

--- frontend-chess/src/components/ChessPieces/Knight.js
+++ frontend-chess/src/components/ChessPieces/Knight.js
@@ -5,9 +5,33 @@ class Knight extends Piece {
         super(color, pos, imgURL);
     }
 
-    showMovement(board=null) {
-        var arr= [];
-        return;
+    getMovementOptions(board=null, playerPiece=true) {
+        /*
+            Parameters:
+                board: Array[Square]
+                playerPiece: bool
+            Returns: Array[] of the positions on the board
+        */
+
+        const pos = this.getPos();
+        console.log(pos)
+        var pieceRow = pos[0];
+        var pieceCol = pos[1];
+
+        var moveOptions = [];
+        
+        // find squares where bishop can move
+        const dirOptions = [[2, 1], [2, -1], [-2, -1], [-2, 1], [1, 2], [1, -2], [-1, -2], [-1, 2]];
+        for(var i=0;i<dirOptions.length;i++) {
+            var newPos = [pieceRow-dirOptions[i][0], pieceCol-dirOptions[i][1]];
+
+            // condition ot check if not out of bounds and if there no piece in the square we can move the knight there
+            // if there's a piece with the opposite color there then we can move the knight as well
+            if(!this.outOfBounds(newPos) && (!this.pieceExists(board, newPos) || this.hasOppPieceColor(board, newPos))) {
+                moveOptions.push(newPos);
+            }
+        }
+        return moveOptions;
     }
 }
 
