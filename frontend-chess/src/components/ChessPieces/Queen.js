a0766f1f411f668f757e745b9e1fda2a59f7adf9
calee14 <caplee1000@gmail.com>
2021-11-17 11:23:35 -0800

    attack pos for queen

--- frontend-chess/src/components/ChessPieces/Queen.js
+++ frontend-chess/src/components/ChessPieces/Queen.js
@@ -5,11 +5,11 @@ class Queen extends Piece {
         super(color, pos, imgURL);
     }
 
-    getMovementOptions(board=null, playerPiece=true) {
+    getMovementOptions(board=null, isPlayerPiece=true) {
         /*
             Parameters:
                 board: Array[Square]
-                playerPiece: bool
+                isPlayerPiece: bool
             Returns: Array[] of the positions on the board
         */
 
@@ -37,6 +37,40 @@ class Queen extends Piece {
             }
         }
         return moveOptions;
+    }
+
+    getAttackPos(board=null, isPlayerPiece=false) {
+        /*
+            Parameters:
+                board: Array[Square]
+                isPlayerPiece: bool
+            Returns: Array[] of the positions on the board
+        */
+
+            const pos = this.getPos();
+            console.log(pos);
+            var pieceRow = pos[0];
+            var pieceCol = pos[1];
+    
+            var attackOptions = [];
+            
+            // find squares where queen can attack
+    
+            const dirOptions = [[0, 1], [0, -1], [-1, 0], [1, 0], [1, 1], [1, -1], [-1, -1], [-1, 1]]; // direction of the rook
+            // loop through each direction and mark every square in that direction
+            for(var i=0;i<dirOptions.length;i++) {
+                var newPos = [pieceRow-dirOptions[i][0], pieceCol-dirOptions[i][1]];
+                while(!this.outOfBounds(newPos) && !this.pieceExists(board, newPos)) { // stop loop when we reach a piece
+                    console.log(newPos);
+                    attackOptions.push(newPos);
+                    newPos = [newPos[0]-dirOptions[i][0], newPos[1]-dirOptions[i][1]];
+                }
+                // add the movement option if the queen can take an opponent's piece
+                if(!this.outOfBounds(newPos)) {
+                    attackOptions.push(newPos);
+                }
+            }
+            return attackOptions;
     }
 }
 
