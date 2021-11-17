04a8abd0ec39656373d15318ecd93e38cdcf933b
calee14 <caplee1000@gmail.com>
2021-11-17 11:23:15 -0800

    attack pos for bishop

--- frontend-chess/src/components/ChessPieces/Bishop.js
+++ frontend-chess/src/components/ChessPieces/Bishop.js
@@ -5,11 +5,11 @@ class Bishop extends Piece {
         super(color, pos, imgURL);
     }
 
-    getMovementOptions(board=null, playerPiece=true) {
+    getMovementOptions(board=null, isPlayerPiece=true) {
         /*
             Parameters:
                 board: Array[Square]
-                playerPiece: bool
+                isPlayerPiece: bool
             Returns: Array[] of the positions on the board
         */
 
@@ -34,6 +34,37 @@ class Bishop extends Piece {
             }
         }
         return moveOptions;
+    }
+
+    getAttackPos(board=null, isPlayerPiece=false) {
+        /*
+            Parameters:
+                board: Array[Square]
+                isPlayerPiece: bool
+            Returns: Array[] of the attack positions on the board
+        */
+
+            const pos = this.getPos();
+            console.log(pos);
+            var pieceRow = pos[0];
+            var pieceCol = pos[1];
+    
+            var attackOptions = [];
+            
+            // find squares where bishop can attack
+            const dirOptions = [[1, 1], [1, -1], [-1, -1], [-1, 1]];
+            for(var i=0;i<dirOptions.length;i++) {
+                var newPos = [pieceRow-dirOptions[i][0], pieceCol-dirOptions[i][1]];
+                while(!this.outOfBounds(newPos) && !this.pieceExists(board, newPos)) {
+                    console.log(newPos);
+                    attackOptions.push(newPos);
+                    newPos = [newPos[0]-dirOptions[i][0], newPos[1]-dirOptions[i][1]];
+                }
+                if(!this.outOfBounds(newPos)) {
+                    attackOptions.push(newPos);
+                }
+            }
+            return attackOptions;
     }
 }
 
