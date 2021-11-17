e75413971597073353cde42a8ab89764394091eb
calee14 <caplee1000@gmail.com>
2021-11-17 11:23:21 -0800

    attack pos for king

--- frontend-chess/src/components/ChessPieces/King.js
+++ frontend-chess/src/components/ChessPieces/King.js
@@ -5,7 +5,7 @@ class King extends Piece {
         super(color, pos, imgURL);
     }
 
-    getMovementOptions(board=null, playerPiece=true) {
+    getMovementOptions(board=null, isPlayerPiece=true) {
         /*
             Parameters:
                 board: Array[Square]
@@ -32,6 +32,35 @@ class King extends Piece {
             }
         }
         return moveOptions;
+    }
+
+    getAttackPos(board=null, isPlayerPiece=false) {
+        /*
+            Parameters:
+                board: Array[Square]
+                playerPiece: bool
+            Returns: Array[] of the positions on the board
+        */
+
+            const pos = this.getPos();
+            console.log(pos)
+            var pieceRow = pos[0];
+            var pieceCol = pos[1];
+    
+            var attackOptions = [];
+            
+            // find squares where bishop can move
+            const dirOptions = [[1, 1], [0, 1], [-1, 1], [0, -1], [-1, -1], [-1, 0], [1, 0], [1, -1]];
+            for(var i=0;i<dirOptions.length;i++) {
+                var newPos = [pieceRow-dirOptions[i][0], pieceCol-dirOptions[i][1]];
+    
+                // condition ot check if not out of bounds and if there no piece in the square we can move the knight there
+                // if there's a piece with the opposite color there then we can move the knight as well
+                if(!this.outOfBounds(newPos)) {
+                    attackOptions.push(newPos);
+                }
+            }
+            return attackOptions;
     }
 }
 
