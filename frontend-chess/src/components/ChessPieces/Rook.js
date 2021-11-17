6d6d5e752f0612fe86751c8266fd44615f24b10b
calee14 <caplee1000@gmail.com>
2021-11-17 11:23:40 -0800

    attack pos for rook

--- frontend-chess/src/components/ChessPieces/Rook.js
+++ frontend-chess/src/components/ChessPieces/Rook.js
@@ -5,11 +5,11 @@ class Rook extends Piece {
         super(color, pos, imgURL);
     }
 
-    getMovementOptions(board=null, playerPiece=true) {
+    getMovementOptions(board=null, isPlayerPiece=true) {
         /*
             Parameters:
                 board: Array[Square]
-                playerPiece: bool
+                isPlayerPiece: bool
             Returns: Array[] of the positions on the board
         */
 
@@ -33,6 +33,40 @@ class Rook extends Piece {
             }
             // add the movement option if the rook can take an opponent's piece
             if(!this.outOfBounds(newPos) && this.pieceExists(board, newPos) && this.hasOppPieceColor(board, newPos)) {
+                moveOptions.push(newPos);
+            }
+        }
+        return moveOptions;
+    }
+
+    getAttackPos(board=null, isPlayerPiece=false) {
+        /*
+            Parameters:
+                board: Array[Square]
+                isPlayerPiece: bool
+            Returns: Array[] of the attack positions on the board
+        */
+
+        const pos = this.getPos();
+        console.log(pos);
+        var pieceRow = pos[0];
+        var pieceCol = pos[1];
+
+        var moveOptions = [];
+        
+        // find squares where rook can move
+
+        const dirOptions = [[0, 1], [0, -1], [-1, 0], [1, 0]]; // direction of the rook
+        // loop through each direction and mark every square in that direction
+        for(var i=0;i<dirOptions.length;i++) {
+            var newPos = [pieceRow-dirOptions[i][0], pieceCol-dirOptions[i][1]];
+            while(!this.outOfBounds(newPos) && !this.pieceExists(board, newPos)) { // stop loop when we reach a piece
+                console.log(newPos);
+                moveOptions.push(newPos);
+                newPos = [newPos[0]-dirOptions[i][0], newPos[1]-dirOptions[i][1]];
+            }
+            // add the movement option if the rook can take an opponent's piece
+            if(!this.outOfBounds(newPos)) {
                 moveOptions.push(newPos);
             }
         }
